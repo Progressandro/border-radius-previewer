28e4507e71ea0d33d4d98696c42de136c210b11c
Alejandro F <progressandro@gmail.com>
2022-06-08 00:04:14 -0300

    Chakra UI.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.js
+import theme from '@/styles/theme'
+import { ChakraProvider } from '@chakra-ui/react'
+import { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
